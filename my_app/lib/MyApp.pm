86a6c40a3df170b6e2b1cd34141838bb584d5d59
Stefan Adams <stefan@adams.fm>
2024-07-13 14:25:08 -0500

    handle barcode input and open and close the door

--- my_app/lib/MyApp.pm
+++ my_app/lib/MyApp.pm
@@ -10,12 +10,25 @@ sub startup ($self) {
   # Configure the application
   $self->secrets($config->{secrets});
 
+  my $store_status = 'closed';
+  $self->helper(
+    store_status => sub ($c, $status=undef) {
+      if ($status) {
+        $store_status = $status;
+      }
+      return $store_status;
+    }
+  );
+
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
   $r->get('/')->to('Example#welcome');
-  $r->post('/barcode')->to('Example#barcode');
+  $r->post('/get_barcode')->to('Example#get_barcode');
+  $r->post('/open_store')->to('Example#open_store');
+  $r->post('/close_store')->to('Example#close_store');
+  $r->post('/store_status')->to('Example#store_status');
 }
 
 1;
--- my_app/lib/MyApp/Controller/Example.pm
+++ my_app/lib/MyApp/Controller/Example.pm
@@ -1,8 +1,33 @@
 package MyApp::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
-sub barcode ($self) {
-  $self->render(text => $self->param('input'));
+sub close_store ($self) {
+  $self->app->store_status('closed');
+  $self->render(json => {
+    status => 'success',
+    message => 'Close Store',
+  });
+}
+
+sub get_barcode ($self) {
+  $self->render(json => {
+    barcode => $self->req->json('/barcode'),
+    name => 'Test',
+  });
+}
+
+sub open_store ($self) {
+  $self->app->store_status('open');
+  $self->render(json => {
+    status => 'success',
+    message => 'Open Store',
+  });
+}
+
+sub store_status ($self) {
+  $self->render(json => {
+    status => $self->app->store_status,
+  });
 }
 
 # This action will render a template
